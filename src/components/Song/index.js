f6620311324970adabc648a56e9754d71566471c
Daniel Martinez <danielm@ornsoft.com>
2022-03-27 18:57:35 -0400

    tooltip

--- src/components/Song/index.js
+++ src/components/Song/index.js
@@ -1,31 +1,43 @@
 import { useState } from "react";
+import { Tooltip, OverlayTrigger } from "react-bootstrap";
 
 const Song = ({ song, active }) => {
   const [favorited, setFavorited] = useState(false);
 
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props} className="toolip"> 
+      MARK AS FAVORITE
+    </Tooltip>
+  );
+
   return (
-      <div className={active ? 'row song border-bottom border-secondary' : 'hidden'}>
-        <div className="track-number">
-          {song.song_order}
+    <div className={active ? "row song" : "hidden"}>
+      <div className="track-number">{song.song_order}</div>
+      <OverlayTrigger
+        placement="top"
+        delay={{ show: 250, hide: 200 }}
+        overlay={renderTooltip}
+      >
+        <div
+          className={favorited ? "favorite favorited" : "favorite"}
+          onClick={() => setFavorited(!favorited)}
+        >
+          ★
         </div>
-        <div className={favorited ? "favorite favorited" : "favorite"} onClick={() => setFavorited(!favorited)}>★</div>
-        <div className="song-name">{song.song_name}</div>
-        {song.song_label && song.song_label.includes("explicit") ? (
-          <div className="label">
-            EXPLICIT
-          </div>
-        ) : (
-          ""
-        )}
-        {song.song_label && song.song_label.includes("upbeat") ? (
-          <div className="label">
-            UPBEAT
-          </div>
-        ) : (
-          ""
-        )}
-        <div className="track-time">{song.song_duration}</div>
-      </div>
+      </OverlayTrigger>
+      <div className="song-name">{song.song_name}</div>
+      {song.song_label && song.song_label.includes("explicit") ? (
+        <div className="label">EXPLICIT</div>
+      ) : (
+        ""
+      )}
+      {song.song_label && song.song_label.includes("upbeat") ? (
+        <div className="label">UPBEAT</div>
+      ) : (
+        ""
+      )}
+      <div className="track-time">{song.song_duration}</div>
+    </div>
   );
 };
 
